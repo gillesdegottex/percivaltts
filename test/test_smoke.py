4b5ac22dbbec672dce02774441f257b085a4422a
Gilles Degottex <gilles.degottex@gmail.com>
2018-02-26 00:41:23 +0100

    adds

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -30,28 +30,33 @@ class TestSmoke(unittest.TestCase):
     def test_data(self):
         import data
 
-        lines = data.loadids(cptest+'file_id_list.scp')
+        fbases = data.loadids(cptest+'file_id_list.scp')
 
         path, shape = data.getpathandshape('dummy.fwspec')
         self.assertTrue(path=='dummy.fwspec')
         self.assertTrue(shape==None)
         path, shape = data.getpathandshape('dummy.fwspec:(-1,129)')
         self.assertTrue(path=='dummy.fwspec')
-        self.assertTrue(shape==[-1,129])
-        path, shape = data.getpathandshape('dummy.fwspec:(-1,129)', [-1,12])
+        self.assertTrue(shape==(-1,129))
+        path, shape = data.getpathandshape('dummy.fwspec:(-1,129)', (-1,12))
         self.assertTrue(path=='dummy.fwspec')
-        self.assertTrue(shape==[-1,12])
-        path, shape = data.getpathandshape('dummy.fwspec', [-1,12])
+        self.assertTrue(shape==(-1,12))
+        path, shape = data.getpathandshape('dummy.fwspec', (-1,12))
         self.assertTrue(path=='dummy.fwspec')
-        self.assertTrue(shape==[-1,12])
+        self.assertTrue(shape==(-1,12))
         dim = data.getlastdim('dummy.fwspec')
         self.assertTrue(dim==1)
         dim = data.getlastdim('dummy.fwspec:(-1,129)')
         self.assertTrue(dim==129)
 
-        # data.load TODO
-        # data.gettotallen TODO
-        # data.cropsize TODO
+        Xs = data.load(cptest+'binary_label_601/*.lab:(-1,601)', fbases, shape=None, frameshift=0.005, verbose=1, label='testload ')
+        self.assertTrue(len(Xs)==10)
+        self.assertTrue(Xs[0].shape==(666, 601))
+
+        self.assertTrue(data.gettotallen(Xs)==5688)
+
+        Xs, Xs = data.cropsize([Xs, Xs]) # TODO Crop against features
+
         # data.cropsilences TODO
         # data.vstack_masked TODO
         # data.maskify TODO
