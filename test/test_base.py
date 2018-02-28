405ed0b1cf98a4ca0e33435373e4d1808eaf3858
Gilles Degottex <gilles.degottex@gmail.com>
2018-02-28 23:47:58 +0100

    create base material for testing

---
+++ test/test_base.py
@@ -0,0 +1,18 @@
+# http://pymbook.readthedocs.io/en/latest/testing.html
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import unittest
+
+cptest = 'test/slttest/'
+
+class TestBase(unittest.TestCase):
+
+    def test_base(self):
+        import run
+        run.features_extraction()
+        run.composition()
+
+if __name__ == '__main__':
+    unittest.main()
