a81fad3454a0ca2964f9fbf12f2742b7966f54c9
Gilles Degottex <gilles.degottex@gmail.com>
2018-03-04 23:57:57 +0100

    use cpu-based environment for testing

---
+++ setenv_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set the environment variables before calling the python code
+
+CONDAPATH=$HOME/miniconda
+
+export PYTHONPATH=$CONDAPATH/lib/python2.7/site-packages:$PYTHONPATH
+export PATH=$CONDAPATH/bin:$PATH
+export LD_LIBRARY_PATH=$CONDAPATH/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$CONDAPATH/lib:$LIBRARY_PATH
+export CPATH="$CONDAPATH/include:$CPATH"
+
+export THEANO_FLAGS="floatX=float32,on_unused_input=ignore,"
+
+export THEANO_FLAGS="device=cpu,exception_verbosity=high,optimizer=None,"$THEANO_FLAGS
+
+unset LD_PRELOAD
+
+python $@
