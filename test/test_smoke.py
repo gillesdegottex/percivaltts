b3c9a5d6954a9149e031def2e8c5bdc13ebdb9d7
Gilles Degottex <gilles.degottex@gmail.com>
2018-03-04 16:08:57 +0100

    test compose

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -91,5 +91,36 @@ class TestSmoke(unittest.TestCase):
         std = data.prediction_std(mod, Xs)
         print(std)
 
+    def test_compose(self):
+        import compose
+
+        cp = 'test/slttest/' # The main directory where the data of the voice is stored
+        fileids = cp+'/file_id_list.scp'
+
+        lab_size = 601
+        spec_size = 65
+        nm_size = 17
+
+        wav_dir = 'wav'
+        f0_path = cp+wav_dir+'_lf0/*.lf0'
+        spec_path = cp+wav_dir+'_fwspec'+str(spec_size)+'/*.fwspec'
+        nm_path = cp+wav_dir+'_fwnm'+str(nm_size)+'/*.fwnm'
+
+        compose.compose([cp+'binary_label_'+str(lab_size)+'/*.lab:(-1,'+str(lab_size)+')'], fileids, 'test/test_made__smoke_test_compose_compose_lab1/*.lab', id_valid_start=8, normfn=compose.normalise_minmax, do_finalcheck=True, wins=[], dropzerovardims=False)
+
+        compose.compose([cp+'binary_label_'+str(lab_size)+'/*.lab:(-1,'+str(lab_size)+')'], fileids, 'test/test_made__smoke_test_compose_compose_lab2/*.lab', id_valid_start=8, normfn=compose.normalise_minmax, do_finalcheck=True, wins=[], dropzerovardims=True)
+
+        compose.compose([f0_path, spec_path+':(-1,'+str(spec_size)+')', nm_path+':(-1,'+str(nm_size)+')'], fileids, 'test/test_made__smoke_test_compose_compose2_cmp1/*.cmp', id_valid_start=8, normfn=compose.normalise_minmax, do_finalcheck=True, wins=[])
+
+        compose.compose([f0_path, spec_path+':(-1,'+str(spec_size)+')', nm_path+':(-1,'+str(nm_size)+')'], fileids, 'test/test_made__smoke_test_compose_compose2_cmp2/*.cmp', id_valid_start=8, normfn=compose.normalise_meanstd, do_finalcheck=True, wins=[])
+
+        compose.compose([f0_path, spec_path+':(-1,'+str(spec_size)+')', nm_path+':(-1,'+str(nm_size)+')'], fileids, 'test/test_made__smoke_test_compose_compose2_cmp3/*.cmp', id_valid_start=8, normfn=compose.normalise_meanstd_bndminmaxm11, do_finalcheck=True, wins=[])
+
+        compose.compose([f0_path, spec_path+':(-1,'+str(spec_size)+')', nm_path+':(-1,'+str(nm_size)+')'], fileids, 'test/test_made__smoke_test_compose_compose2_cmp4/*.cmp', id_valid_start=8, normfn=compose.normalise_meanstd_bndnmnoscale, do_finalcheck=True, wins=[])
+
+        compose.compose([f0_path, spec_path+':(-1,'+str(spec_size)+')', nm_path+':(-1,'+str(nm_size)+')'], fileids, 'test/test_made__smoke_test_compose_compose2_cmp4/*.cmp', id_valid_start=8, normfn=compose.normalise_meanstd_bndnmnoscale, do_finalcheck=True, wins=[[-0.5, 0.0, 0.5], [1.0, -2.0, 1.0]])
+
+        compose.create_weights(spec_path+':(-1,'+str(spec_size)+')', fileids, 'test/test_made__smoke_test_compose_compose2_w1/*.w', spec_type='fwspec', thresh=-32)
+
 if __name__ == '__main__':
     unittest.main()
