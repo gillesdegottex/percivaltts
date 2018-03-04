f5cc6ff2e75ff62d84bcb9b1af20b409e0980647
Gilles Degottex <gilles.degottex@gmail.com>
2018-03-04 23:43:12 +0100

    fix

--- test/test_smoke_theano.py
+++ test/test_smoke_theano.py
@@ -7,11 +7,44 @@ import unittest
 
 class TestSmokeTheano(unittest.TestCase):
     def test_model(self):
+
+        import model
         class ModelSmoke(model.Model):
             def train(self, params, indir, outdir, outwdir, fid_lst_tra, fid_lst_val, X_vals, Y_vals, cfg, params_savefile, trialstr='', cont=None):
                 raise ValueError('That\'s a smoky model that doesn\'t train anything')
 
         mod = ModelSmoke()
+
+        # def nbParams(self) # TODO
+
+        # def saveAllParams(self, fmodel, cfg=None, extras=dict(), printfn=print) # TODO
+
+        # def loadAllParams(self, fmodel, printfn=print) # TODO
+
+        # def saveTrainingState(self, fstate, cfg=None, extras=dict(), printfn=print) # TODO
+
+        # def loadTrainingState(self, fstate, cfg, printfn=print) # TODO
+
+        # def randomize_hyper(self, cfg) # TODO
+
+        # def train_multipletrials(self, indir, outdir, outwdir, fid_lst_tra, fid_lst_val, params, params_savefile, cfgtomerge=None, cont=None, **kwargs) # TODO
+
+        # def generate(self, params_savefile, outsuffix, cfg, do_objmeas=True, do_resynth=True, indicestosynth=None
+        #         , spec_comp='fwspec'
+        #         , spec_size=129
+        #         , nm_size=33
+        #         , do_mlpg=False
+        #         , pp_mcep=False
+        #         , pp_spec_pf_coef=-1 # Common value is 1.2
+        #         , pp_spec_extrapfreq=-1
+        #         ) # TODO
+
+        modgan = ModelGAN(601, 65, 17)
+
+        cfg = configuration() # Init configuration structure
+        cfg.print_content()
+        modgan.train_multipletrials(cfg.indir, cfg.outdir, cfg.outwdir, fid_lst_tra, fid_lst_val, mod.params_trainable, 'smokymodelparams.pkl', cfgtomerge=cfg, cont=cont) # TODO
+
 
     def test_utils_theano(self):
         import utils_theano
