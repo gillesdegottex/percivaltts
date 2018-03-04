3067f21d44a3c824227a1f135770f79468ede057
Gilles Degottex <gilles.degottex@gmail.com>
2018-03-04 16:45:42 +0100

    mv weights_normal_ortho

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -5,23 +5,38 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import unittest
 
+import numpy as np
+
 cptest = 'test/slttest/'
 
 class TestSmoke(unittest.TestCase):
     def test_utils(self):
         import utils
+
         cfg = utils.configuration()
+
         utils.print_log('print_log')
+
         utils.print_tty('print_tty', end='\n')
+
         print(utils.datetime2str(sec=1519426184))
+
         print(utils.time2str(sec=1519426184))
+
         utils.makedirs('/dev/null')
+
         self.assertTrue(utils.is_int('74324'))
         self.assertFalse(utils.is_int('743.24'))
+
+        rng = np.random.RandomState(123)
+        utils.weights_normal_ortho(32, 64, 1.0, rng, dtype=np.float32)
+
         memres = utils.proc_memresident()
         print(memres)
         self.assertNotEqual(memres, -1)
+
         utils.print_sysinfo()
+
         # utils.print_sysinfo_theano() TODO
         # utils.log_plot_costs(costs_tra, costs_val, worst_val, fname, epochs_modelssaved, costs_discri=[]) TODO
         # utils.log_plot_costs(costs, worst_val, fname, epochs_modelssaved) TODO
