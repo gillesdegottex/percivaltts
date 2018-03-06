b8729e421b8ad9c73cbdd236aeebb161e7561d93
Gilles Degottex <gilles.degottex@gmail.com>
2018-03-06 21:39:31 +0100

    test fwcep fwspec in weights computation

--- test/test_base.py
+++ test/test_base.py
@@ -35,7 +35,8 @@ out_size = 1+spec_size+nm_size
 wav_dir = 'wav'
 wav_path = cp+wav_dir+'/*.wav'
 f0_path = cp+wav_dir+'_lf0/*.lf0'
-spec_path = cp+wav_dir+'_fwspec'+str(spec_size)+'/*.fwspec'
+spec_fw_path = cp+wav_dir+'_fwspec'+str(spec_size)+'/*.fwspec'
+spec_fwcep_path = cp+wav_dir+'_fwcep'+str(spec_size)+'/*.mcep'
 nm_path = cp+wav_dir+'_fwnm'+str(nm_size)+'/*.fwnm'
 cfg.outdir = cp+wav_dir+'_cmp_lf0_fwspec'+str(spec_size)+'_fwnm'+str(nm_size)+'_bndnmnoscale/*.cmp:(-1,'+str(out_size)+')'
 cfg.wdir = cp+wav_dir+'_fwspec'+str(spec_size)+'_weights/*.w:(-1,1)'
@@ -51,7 +52,7 @@ class TestBase(unittest.TestCase):
             for fid in fids:
                 print('Extracting features from: '+fid)
                 pulsemodel.analysisf(wav_path.replace('*',fid), f0_min=f0_min, f0_max=f0_max, f0_file=f0_path.replace('*',fid), f0_log=True,
-                spec_file=spec_path.replace('*',fid), spec_nbfwbnds=spec_size, nm_file=nm_path.replace('*',fid), nm_nbfwbnds=nm_size, verbose=1)
+                spec_file=spec_fw_path.replace('*',fid), spec_nbfwbnds=spec_size, nm_file=nm_path.replace('*',fid), nm_nbfwbnds=nm_size, verbose=1)
 
 
         import compose
@@ -61,12 +62,31 @@ class TestBase(unittest.TestCase):
         compose.compose([label_path+':(-1,'+str(in_size)+')'], cfg.fileids, cfg.indir, id_valid_start=cfg.id_valid_start, normfn=compose.normalise_minmax, do_finalcheck=True, wins=[])
 
         # Compose the outputs
-        compose.compose([f0_path, spec_path+':(-1,'+str(spec_size)+')', nm_path+':(-1,'+str(nm_size)+')'], cfg.fileids, cfg.outdir, id_valid_start=cfg.id_valid_start, normfn=compose.normalise_meanstd_bndnmnoscale)
+        compose.compose([f0_path, spec_fw_path+':(-1,'+str(spec_size)+')', nm_path+':(-1,'+str(nm_size)+')'], cfg.fileids, cfg.outdir, id_valid_start=cfg.id_valid_start, normfn=compose.normalise_meanstd_bndnmnoscale)
 
         # Create time weights (column vector in [0,1]). The frames at begining or end of
         # each file whose weights are smaller than 0.5 will be ignored by the training
-        compose.create_weights(spec_path+':(-1,'+str(spec_size)+')', cfg.fileids, cfg.wdir)
+        compose.create_weights(spec_fw_path+':(-1,'+str(spec_size)+')', cfg.fileids, cfg.wdir, spec_type='mcep')    # Wrong data, just to smoke it
+        compose.create_weights(spec_fw_path+':(-1,'+str(spec_size)+')', cfg.fileids, cfg.wdir, spec_type='fwcep')   # Wrong data, just to smoke it
+        compose.create_weights(spec_fw_path+':(-1,'+str(spec_size)+')', cfg.fileids, cfg.wdir, spec_type='fwspec')  # Overwrite with the good one
 
+    # def test_vocoder_pulsemodel_features_extraction_and_composition_fwcep(self):
+    #     import pulsemodel
+    #     with open(cfg.fileids) as f:
+    #         fids = filter(None, [x for x in map(str.strip, f.readlines()) if x])
+    #         for fid in fids:
+    #             print('Extracting features from: '+fid)
+    #             pulsemodel.analysisf(wav_path.replace('*',fid), f0_min=f0_min, f0_max=f0_max, f0_file=f0_path.replace('*',fid), f0_log=True,
+    #             spec_file=spec_fwcep_path.replace('*',fid), spec_fwceporder=spec_size, nm_file=nm_path.replace('*',fid), nm_nbfwbnds=nm_size, verbose=1)
+    #
+    #     import compose
+    #
+    #     # Compose the outputs for fwcep
+    #     compose.compose([f0_path, spec_fwcep_path+':(-1,'+str(spec_size)+')', nm_path+':(-1,'+str(nm_size)+')'], cfg.fileids, cp+wav_dir+'_cmp_lf0_fwcep'+str(spec_size)+'_fwnm'+str(nm_size)+'_bndnmnoscale/*.cmp:(-1,'+str(out_size)+')', id_valid_start=cfg.id_valid_start, normfn=compose.normalise_meanstd_bndnmnoscale)
+    #
+    #     # Create time weights (column vector in [0,1]). The frames at begining or end of
+    #     # each file whose weights are smaller than 0.5 will be ignored by the training
+    #     compose.create_weights(spec_fwcep_path+':(-1,'+str(spec_size)+')', cfg.fileids, cfg.wdir, spec_type='fwcep')
 
 if __name__ == '__main__':
     unittest.main()
