2c7576f5871cd39bed5195ad9a83d79cc1afc6d6
Gilles Degottex <gilles.degottex@gmail.com>
2018-03-07 16:11:59 +0100

    make utils a commonly included module

--- test/test_smoke_theano.py
+++ test/test_smoke_theano.py
@@ -3,6 +3,9 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))+'/external/')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))+'/external/Lasagne/')
+
 import unittest
 
 from utils import *
