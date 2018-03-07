5e32c67daa92fe0c404929c0d0c00179ed04b96d
Gilles Degottex <gilles.degottex@gmail.com>
2018-03-07 16:10:28 +0100

    make utils a commonly included module

--- test/test_base.py
+++ test/test_base.py
@@ -3,9 +3,11 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))+'/external/')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))+'/external/Lasagne/')
+
 import unittest
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))+'/external/')
 from utils import *
 print_sysinfo()
 
