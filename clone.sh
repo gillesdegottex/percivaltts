f9da3e25d7c84f93fe6bfebb87beb1403cd9d389
Gilles Degottex <gilles.degottex@gmail.com>
2018-04-06 10:21:58 +0200

    doc

--- clone.sh
+++ clone.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# This script is for cloning the code in an "experiment" directory, and run it.
-# By cloning the code, you also keep an original copy of the experiment's code
-# while you can work/run other experiments in parallel without losing track of
-# what has been changed in between.
+# This script clones the code in an "experiment" directory EXPDIR, and run it.
+# By cloning the code, you keep an original copy of the experiment that is
+# running while you can continue working on other experiments in parallel
+# without losing track of what has been changed in the meantime.
 
-# The output of the experiment will appear in a sub-directory "out" along side
-# the directory "code", the clone source code.
+# The output of the experiment will appear in a sub-directory OUTDIR along side
+# the CODEDIR directory, the cloned source code.
 
-# This is also useful for running variants of an experiment by copying the whole
-# working directory (the parent dir of "Code" and "out") and modifying only a
-# single parameter.
+# This is also useful for running slight variants of an experiment by copying
+# the whole experiment directory (the parent dir of CODEDIR and OUTDIR) and
+# modifying only a single parameter.
 
+# Example
+#  ./clone.sh /data/research/exp1 bash setenv.sh run.py
+# which clones the current directory into /data/research/exp1/percival; cd
+# into this same directory and run the command "$ bash setenv.sh run.py"
 
 #Copyright(C) 2017 Engineering Department, University of Cambridge, UK.
 #
@@ -28,60 +32,65 @@
 #
 #Author
 #   Gilles Degottex <gad27@cam.ac.uk>
-#
 
-WORKDIR=$1
+# The root experiment directory where CODEDIR and OUTDIR will appear.
+EXPDIR=$1
+
+# This two variables are hard coded because all the arguments after $1 is
+# the command to run.
 CODEDIR=percival
 OUTDIR=out
 
+# The source code is where clone.sh is. Hack CODESRCDIR if you want otherwise.
 CODESRCDIR=$(dirname "$0")              # relative
 CODESRCDIR=$( ( cd "$CODESRCDIR" && pwd ) )
-echo -n Cloning \"$CODESRCDIR\" in \"$WORKDIR\"
+echo -n Cloning \"$CODESRCDIR\" in \"$EXPDIR\"
 
-# Create the destination directory if access done by NFS
-if [[ $WORKDIR = '/'* ]] ; then
+# Create the destination directory
+if [[ $EXPDIR = '/'* ]] ; then
     echo " (using file system)"
-    mkdir -p $WORKDIR;
-    mkdir -p $WORKDIR/$OUTDIR;
+    mkdir -p $EXPDIR;
+    mkdir -p $EXPDIR/$OUTDIR;
 else
-    echo " (using ssh on ${WORKDIR%:*})"
-    ssh ${WORKDIR%:*} /bin/mkdir -p ${WORKDIR#*:}/$OUTDIR;
+    echo " (using ssh on ${EXPDIR%:*})"
+    ssh ${EXPDIR%:*} /bin/mkdir -p ${EXPDIR#*:}/$OUTDIR;
 fi
 
-# Do the actual copy
-# cp -fr $CODESRCDIR $WORKDIR
-rsync -qav $CODESRCDIR/ $WORKDIR/$CODEDIR/ --exclude .git --exclude .git/
+# Do the actual copy using rsync to avoid re-copying files already copied
+# from any previous cloning.
+# cp -fr $CODESRCDIR $EXPDIR
+rsync -qav $CODESRCDIR/ $EXPDIR/$CODEDIR/ --exclude .git --exclude .git/
 
 
-# Copy a shallow version of the git in the code directory
+# Copy a shallow version of any .git directory in the code directory
 if [[ -d '.git/' ]] ; then
     echo "Copy a shallow git in the code directory"
     rm -fr tmp_clone_gitstatedepth1;
     mkdir -p tmp_clone_gitstatedepth1;
     cd tmp_clone_gitstatedepth1;
     git clone --depth 1 --quiet file://$CODESRCDIR gitstatedepth1;
-    rsync -qav gitstatedepth1/.git/ $WORKDIR/$CODEDIR/.git/;
+    rsync -qav gitstatedepth1/.git/ $EXPDIR/$CODEDIR/.git/;
     cd ..;
     rm -fr tmp_clone_gitstatedepth1;
 fi
 
-
+# Run the command that is after the clone.sh arguments
 if [[ "${@:2}" ]]; then
 
-    if [[ $WORKDIR = '/'* ]] ; then
+    if [[ $EXPDIR = '/'* ]] ; then
         echo Run command: "${@:2}" " (using file system)"
 
         # Go into the output directory
-        cd $WORKDIR/$OUTDIR
+        cd $EXPDIR/$OUTDIR
 
-# Keep it unindented
+# Keep it unindented to avoid undesired empty spaces before the command runned.
 # ${@:2} > log 2>&1
 ${@:2}
 
     else
-        echo Run command: "${@:2}" " (using ssh on ${WORKDIR%:*})"
+        echo Run command: "${@:2}" " (using ssh on ${EXPDIR%:*})"
 
-        ssh ${WORKDIR%:*} "cd ${WORKDIR#*:}/$OUTDIR; ${@:2}"
+        ssh ${EXPDIR%:*} "cd ${EXPDIR#*:}/$OUTDIR; ${@:2}"
 
     fi
 
