117495059af624369a81b7e9dc002df76e4ebb5b
Gilles Degottex <gilles.degottex@gmail.com>
2018-04-06 12:49:22 +0200

    fix

--- setup.py
+++ setup.py
@@ -5,13 +5,38 @@ def readme():
         long_description =  f.read()
 
 setup(name='percival-tts',
-      version='0.9.0',
-      description='Percival and the quest for the holy waveform - Acoustic model for DNN-based TTS',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/gillesdegottex/percival-tts',
-      author='Gilles Degottex',
-      author_email='gad27@cam.ac.uk',
-      license='Apache License (2.0)',
-      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-      zip_safe=False)
+    version='0.9.0',
+    description='Percival and the quest for the holy waveform - Acoustic model for DNN-based TTS',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/gillesdegottex/percival-tts',
+    author='Gilles Degottex',
+    author_email='gad27@cam.ac.uk',
+    license='Apache License (2.0)',
+
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    zip_safe=False
+    )
