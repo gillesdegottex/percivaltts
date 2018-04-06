b454d2f3defe72135c6d6e53ec9148409a990c38
Gilles Degottex <gilles.degottex@gmail.com>
2018-04-06 11:02:59 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='funniest',
-      version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIT',
+setup(name='percival-tts',
+      version='0.9.0',
+      description='Percival and the quest for the holy waveform - Acoustic model for DNN-based TTS',
+      url='https://github.com/gillesdegottex/percival-tts',
+      author='Gilles Degottex',
+      author_email='gad27@cam.ac.uk',
+      license='Apache License (2.0)',
       packages=['funniest'],
       zip_safe=False)
