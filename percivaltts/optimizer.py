c90324eaaa8e1a7d411f6510781315990a3cfa9c
Gilles Degottex <gilles.degottex@gmail.com>
2018-04-12 13:47:39 +0200

    put the discri build in the Model object

--- percivaltts/optimizer.py
+++ percivaltts/optimizer.py
@@ -138,7 +138,7 @@ class Optimizer:
         if self._errtype=='WGAN':
             print('Preparing discriminator for WGAN...')
             discri_input_var = T.tensor3('discri_input') # Either real data to predict/generate, or, fake data that has been generated
-            [discri, layer_discri, layer_cond] = models_cnn.ModelCNN_build_discri(discri_input_var, self._model._input_values, self._model.specsize, self._model.nmsize, self._model.insize, hiddensize=self._model._hiddensize, nbcnnlayers=self._model._nbcnnlayers, nbfilters=self._model._nbfilters, spec_freqlen=self._model._spec_freqlen, nm_freqlen=self._model._nm_freqlen, windur=self._model._windur, use_LSweighting=cfg.train_LScoef>0.0, LSWGANtransflc=self._LSWGANtransflc, LSWGANtransc=self._LSWGANtransc)
+            [discri, layer_discri, layer_cond] = self._model.build_discri(discri_input_var, self._model._input_values, self._model.specsize, self._model.nmsize, self._model.insize, hiddensize=self._model._hiddensize, nbcnnlayers=self._model._nbcnnlayers, nbfilters=self._model._nbfilters, spec_freqlen=self._model._spec_freqlen, nm_freqlen=self._model._nm_freqlen, windur=self._model._windur, use_LSweighting=cfg.train_LScoef>0.0, LSWGANtransflc=self._LSWGANtransflc, LSWGANtransc=self._LSWGANtransc)
 
             print('    Discriminator architecture')
             for l in lasagne.layers.get_all_layers(discri):
