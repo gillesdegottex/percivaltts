3a03f2c4a0514733ee7f965a69e9477951c4b27e
Gilles Degottex <gilles.degottex@gmail.com>
2018-07-22 15:07:32 +0200

    back to original

--- percivaltts/optimizer.py
+++ percivaltts/optimizer.py
@@ -452,7 +452,7 @@ class Optimizer:
         cfg.train_G_adam_beta1 = 0.5            # [potential hyper-parameter]
         cfg.train_G_adam_beta2 = 0.9            # [potential hyper-parameter]
         cfg.train_pg_lambda = 10                # [potential hyper-parameter]
-        cfg.train_LScoef = 0.10                 # If >0, mix LSE and WGAN losses (def. 0.10)
+        cfg.train_LScoef = 0.25                 # If >0, mix LSE and WGAN losses (def. 0.25)
         cfg.train_validation_ltm_winlen = 20    # Now that I'm using min and max epochs, I could use the actuall D cost and not the ltm(D cost) TODO
 
         cfg.train_min_nbepochs = 200
