6e1dd16a0968b2c2cfcccd876eeb3cbd2c374c98
Gilles Degottex <gilles.degottex@gmail.com>
2018-09-06 09:12:43 +0200

    simplify

--- percivaltts/models_generic.py
+++ percivaltts/models_generic.py
@@ -38,9 +38,9 @@ from tensorflow import keras
 
 
 class ModelGeneric(modeltts.ModelTTS):
-    def __init__(self, ctxsize, vocoder, mlpg_wins=[], layertypes=['FC', 'FC', 'FC'], hiddensize=256, nonlinearity=keras.layers.LeakyReLU(alpha=0.3), bn_axis=None, nameprefix=None):
-        if bn_axis is None: bn_axis=-1
-        modeltts.ModelTTS.__init__(self, ctxsize, vocoder, hiddensize)
+    def __init__(self, ctxsize, vocoder, cfgarch, layertypes=['FC', 'FC', 'FC'], nameprefix=None):
+        bn_axis=-1
+        modeltts.ModelTTS.__init__(self, ctxsize, vocoder, cfgarch=cfgarch)
 
         if nameprefix is None: nameprefix=''
 
@@ -48,20 +48,23 @@ class ModelGeneric(modeltts.ModelTTS):
         l_out = l_in
 
         for layi in xrange(len(layertypes)):
-            layerstr = nameprefix+'l'+str(1+layi)+'_{}{}'.format(layertypes[layi], hiddensize)
+            layerstr = nameprefix+'l'+str(1+layi)+'_{}{}'.format(layertypes[layi], cfgarch.arch_hiddensize)
 
+            if layertypes[layi]=='DO':
+                l_out = keras.layers.Dropout(0.2, noise_shape=(5, 1, None))(l_out)  # TODO 5
             if layertypes[layi]=='FC':
-                l_out = keras.layers.Dense(hiddensize, use_bias=False, name=layerstr)(l_out)
+                l_out = keras.layers.Dense(cfgarch.arch_hiddensize, use_bias=False, name=layerstr)(l_out) # TODO , kernel_initializer='normal' blows up the values
                 l_out = keras.layers.BatchNormalization(axis=bn_axis)(l_out)
                 l_out = keras.layers.LeakyReLU(alpha=0.3)(l_out)                  # TODO TODO TODO Using nonlinearity doesn't work! Need to clone it maybe
             if layertypes[layi]=='GRU' or layertypes[layi]=='BGRU':
-                l_gru = keras.layers.GRU(hiddensize, activation='tanh', use_bias=True, dropout=0.0, recurrent_dropout=0.0, implementation=1, return_sequences=True, reset_after=False, name=layerstr)
+                l_gru = keras.layers.GRU(cfgarch.arch_hiddensize, activation='tanh', use_bias=True, dropout=0.0, recurrent_dropout=0.0, implementation=1, return_sequences=True, reset_after=False, name=layerstr)
                 if layertypes[layi]=='BGRU':    l_out=keras.layers.Bidirectional(l_gru)(l_out)
                 else:                           l_out=l_gru(l_out)
             if layertypes[layi]=='LSTM' or layertypes[layi]=='BLSTM':
+                # TODO TODO TODO Very very slow!!!
                 # TODO TODO TODO Try CuDNNLSTM
-                # l_out = keras.layers.SimpleRNN(hiddensize, activation='tanh', return_sequences=True)(l_out)
-                l_lstm = keras.layers.LSTM(hiddensize, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True, dropout=0.0, recurrent_dropout=0.0, implementation=1, return_sequences=True, name=layerstr)
+                # l_out = keras.layers.SimpleRNN(cfgarch.arch_hiddensize, activation='tanh', return_sequences=True)(l_out)
+                l_lstm = keras.layers.LSTM(cfgarch.arch_hiddensize, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True, dropout=0.0, recurrent_dropout=0.0, implementation=1, return_sequences=True, name=layerstr)
                 if layertypes[layi]=='BLSTM':   l_out=keras.layers.Bidirectional(l_lstm)(l_out)
                 else:                           l_out=l_lstm(l_out)
                 # TODO TODO TODO Test batch normalisation
@@ -70,9 +73,10 @@ class ModelGeneric(modeltts.ModelTTS):
                 if layertypes[layi][0]=='CNN':
                     nbfilters = layertypes[layi][1]
                     winlen = layertypes[layi][2]
-                    l_out = keras.layers.Conv1D(nbfilters*hiddensize, winlen, strides=1, padding='same', dilation_rate=1, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros')(l_out)
-                    # TODO TODO TODO Test dilation_rate>1 # TODO TODO TODO nbfilters*hiddensize
-                    l_out = keras.layers.LeakyReLU(alpha=0.3)(l_out)                  # TODO only tanh works
+                    l_out = keras.layers.Conv1D(nbfilters*cfgarch.arch_hiddensize, winlen, strides=1, padding='same', dilation_rate=1, activation=None, use_bias=False, kernel_initializer='glorot_uniform', bias_initializer='zeros')(l_out)
+                    # TODO TODO TODO Test dilation_rate>1 # TODO TODO TODO nbfilters*cfgarch.arch_hiddensize
+                    l_out = keras.layers.BatchNormalization(axis=bn_axis)(l_out)
+                    l_out = keras.layers.LeakyReLU(alpha=0.3)(l_out)
                 # if layertypes[layi][0]=='GCNN':
 
         l_out = modeltts.layer_final(l_out, vocoder, mlpg_wins=[])
@@ -82,26 +86,3 @@ class ModelGeneric(modeltts.ModelTTS):
 
         # from keras.utils import plot_model
         # plot_model(self._kerasmodel, to_file='model.png')
-
-    def build_critic(self, nonlinearity=nonlin_very_leaky_rectify, postlayers_nb=6, use_LSweighting=True, LSWGANtransfreqcutoff=4000, LSWGANtranscoef=1.0/8.0, use_WGAN_incnoisefeature=False):
-
-        l_in = keras.layers.Input(shape=(None, self.vocoder.featuressize()), name='input')
-
-        l_in_ctx = keras.layers.Input(shape=(None, self.ctxsize), name='input_ctx')   # TODO TODO TODO
-
-        l_out = keras.layers.Concatenate(axis=-1, name='lo_concatenation')([l_in, l_in_ctx])
-        # l_out = l_in
-
-        l_out = keras.layers.Dense(self.hiddensize, activation=None)(l_out)    # TODO , use_bias=False
-        l_out = keras.layers.LeakyReLU(alpha=0.3)(l_out)           # TODO TODO TODO Using nonlinearity doesn't work! Need to clone it maybe
-
-        l_out = keras.layers.Dense(self.hiddensize, activation=None)(l_out)    # TODO , use_bias=False
-        l_out = keras.layers.LeakyReLU(alpha=0.3)(l_out)           # TODO TODO TODO Using nonlinearity doesn't work! Need to clone it maybe
-
-        l_out = keras.layers.Dense(self.hiddensize, activation=None)(l_out)    # TODO , use_bias=False
-        l_out = keras.layers.LeakyReLU(alpha=0.3)(l_out)           # TODO TODO TODO Using nonlinearity doesn't work! Need to clone it maybe
-
-        l_out = keras.layers.Dense(1, activation=None)(l_out)    # TODO , use_bias=False
-        l_out = keras.layers.LeakyReLU(alpha=0.3)(l_out)           # TODO TODO TODO Using nonlinearity doesn't work! Need to clone it maybe
-
-        return l_in, l_in_ctx, l_out
