fcaf77abe0f6dae32d80f8160e72188674a555f1
Gilles Degottex <gilles.degottex@gmail.com>
2018-10-03 13:12:57 +0200

    add basic functionalities in the module

--- percivaltts/__init__.py
+++ percivaltts/__init__.py
@@ -1,3 +1,5 @@
 # This version number has to be updated for each release
 # (../setup.py reads this file to obtain the version number of the packages)
 __version__ = '2.0.0'
+
+from percivaltts import *
