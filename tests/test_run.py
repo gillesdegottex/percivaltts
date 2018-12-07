479e5edbb9fed57f2c5cdc33da189845515e5bfa
Gilles Degottex <gilles.degottex@gmail.com>
2018-12-07 15:15:42 +0100

    fix

--- tests/test_run.py
+++ tests/test_run.py
@@ -19,30 +19,27 @@ class TestRun(unittest.TestCase):
         os.listdir('tests/slt_arctic_merlin_full')
         os.system('ls -l tests/slt_arctic_merlin_full')
 
-        import run
+        import percivaltts.run as run
 
         print('Overwrite the configuration to run a smoke test')
         run.cfg.id_valid_start = 8
         run.cfg.id_valid_nb = 1
         run.cfg.id_test_nb = 1
-        run.cfg.train_batch_size = 2
+
         run.cfg.train_min_nbepochs = 1
-        run.cfg.train_max_nbepochs = 5
-        run.cfg.train_cancel_nodecepochs = 2
-        run.cfg.model_hiddensize = 4
-        run.cfg.model_nbprelayers = 1
-        run.cfg.model_nbcnnlayers = 1
-        run.cfg.model_nbfilters = 2
-        run.cfg.model_spec_freqlen = 3
-        run.cfg.model_nm_freqlen = 3
-        run.cfg.model_windur = 0.020
+        run.cfg.train_max_nbepochs = 10
+        run.cfg.train_cancel_nodecepochs = 3
+
+        run.cfg.train_batch_size = 2
+        run.cfg.arch_hiddenwidth = 4
+        run.cfg.arch_gen_nbcnnlayers = 1
 
         run.cfg.print_content()
 
         run.features_extraction()
         run.contexts_extraction()
         run.training(cont=False)
-        run.generate('model-last.pkl')
+        run.generate()
 
 if __name__ == '__main__':
     unittest.main()
