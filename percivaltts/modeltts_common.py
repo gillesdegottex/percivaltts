06db644ca7c29627adb84fc8617cbb4c1feccc13
Gilles Degottex <gilles.degottex@gmail.com>
2018-12-07 12:21:10 +0100

    fix paper model

--- percivaltts/modeltts_common.py
+++ percivaltts/modeltts_common.py
@@ -58,6 +58,7 @@ class Generic(modeltts.ModelTTS):
         # plot_model(self._kerasmodel, to_file='model.png')
 
 
+# TODO TODO TODO Replace blocks by block functions
 class DCNNF0SpecNoiseFeatures(modeltts.ModelTTS):
     def __init__(self, ctxsize, vocoder, cfgarch, nameprefix=None):
         bn_axis=-1
@@ -67,32 +68,36 @@ class DCNNF0SpecNoiseFeatures(modeltts.ModelTTS):
 
         l_in = kl.Input(shape=(None, ctxsize), name=nameprefix+'input.conditional')
 
-        l_ctx = networktts.network_context_preproc(l_in, cfgarch.arch_ctx_winlen, cfgarch, bn=True)
+        # l_ctx = networktts.network_context_preproc(l_in, cfgarch.arch_ctx_winlen, cfgarch, bn=True)
         # l_ctx = networktts.GaussianNoiseInput(width=100)(l_ctx)
+        l_ctx = l_in
+        for _ in xrange(cfgarch.arch_ctx_nbcnnlayers):
+            l_ctx = networktts.pCNN1D(l_ctx, cfgarch.arch_hiddenwidth, cfgarch.arch_ctx_winlen)
+        l_ctx = networktts.pFC(l_ctx, cfgarch.arch_hiddenwidth)
+        l_ctx = networktts.pFC(l_ctx, cfgarch.arch_hiddenwidth)
 
-        # F0
+        # F0 TODO TODO TODO Use BLSTM as in the paper
         l_f0 = l_ctx
-        l_f0 = kl.Conv1D(cfgarch.arch_gen_nbfilters, int(0.5*0.200/vocoder.shift)*2+1, strides=1, padding='same', use_bias=False)(l_f0)
-        l_f0 = kl.BatchNormalization(axis=bn_axis)(l_f0)
-        l_f0 = kl.LeakyReLU(alpha=0.3)(l_f0)
+        l_f0 = networktts.pCNN1D(l_f0, cfgarch.arch_hiddenwidth, int(0.5*0.200/vocoder.shift)*2+1) # TODO TODO TODO hardcoded 0.200s
         l_f0 = kl.Dense(1, activation=None, use_bias=True)(l_f0)
 
         # Spec
         l_spec = l_ctx
-        l_spec = kl.Dense(cfgarch.arch_hiddenwidth, use_bias=False)(l_spec)
-        l_spec = kl.BatchNormalization(axis=bn_axis)(l_spec)
-        l_spec = kl.LeakyReLU(alpha=0.3)(l_spec)
-        l_spec = kl.Dense(cfgarch.arch_hiddenwidth, use_bias=False)(l_spec)
-        l_spec = kl.BatchNormalization(axis=bn_axis)(l_spec)
-        l_spec = kl.LeakyReLU(alpha=0.3)(l_spec)
+        # l_spec = kl.Dense(cfgarch.arch_hiddenwidth, use_bias=False)(l_spec)
+        # l_spec = kl.BatchNormalization(axis=bn_axis)(l_spec)
+        # l_spec = kl.LeakyReLU(alpha=0.3)(l_spec)
+        # l_spec = kl.Dense(cfgarch.arch_hiddenwidth, use_bias=False)(l_spec)
+        # l_spec = kl.BatchNormalization(axis=bn_axis)(l_spec)
+        # l_spec = kl.LeakyReLU(alpha=0.3)(l_spec)
 
+        # TODO TODO TODO Use Gated CNN !
         l_spec = kl.Dense(vocoder.specsize(), use_bias=True)(l_spec)   # Projection
         l_spec = kl.Reshape([-1,vocoder.specsize(), 1])(l_spec) # Add the channels after the spectral dimension
         for _ in xrange(cfgarch.arch_gen_nbcnnlayers):
-            l_spec = kl.Conv2D(cfgarch.arch_gen_nbfilters, [cfgarch.arch_winlen,cfgarch.arch_spec_freqlen], strides=(1, 1), padding='same', dilation_rate=(1, 1), use_bias=False, data_format='channels_last')(l_spec)
+            l_spec = kl.Conv2D(cfgarch.arch_gen_nbfilters, [cfgarch.arch_gen_winlen,cfgarch.arch_spec_freqlen], strides=(1, 1), padding='same', dilation_rate=(1, 1), use_bias=False, data_format='channels_last')(l_spec)
             l_spec = kl.BatchNormalization(axis=bn_axis)(l_spec)
             l_spec = kl.LeakyReLU(alpha=0.3)(l_spec)
-        l_spec = kl.Conv2D(1, [cfgarch.arch_winlen,cfgarch.arch_spec_freqlen], strides=(1, 1), padding='same', dilation_rate=(1, 1), use_bias=True, activation=None, data_format='channels_last')(l_spec)
+        l_spec = kl.Conv2D(1, [cfgarch.arch_gen_winlen,cfgarch.arch_spec_freqlen], strides=(1, 1), padding='same', dilation_rate=(1, 1), use_bias=True, activation=None, data_format='channels_last')(l_spec)
         l_spec = kl.Reshape([-1,l_spec.shape[-2]])(l_spec)
 
         # NM
@@ -103,18 +108,16 @@ class DCNNF0SpecNoiseFeatures(modeltts.ModelTTS):
             l_nm = kl.Dense(vocoder.noisesize(), use_bias=True)(l_ctx)   # Projection
             l_nm = kl.Reshape([-1,vocoder.noisesize(), 1])(l_nm) # Add the channels after the spectral dimension
             for _ in xrange(cfgarch.arch_gen_nbcnnlayers/2):
-                l_nm = kl.Conv2D(cfgarch.arch_gen_nbfilters, [cfgarch.arch_winlen,cfgarch.arch_noise_freqlen], strides=(1, 1), padding='same', dilation_rate=(1, 1), use_bias=False, data_format='channels_last')(l_nm)
+                l_nm = kl.Conv2D(cfgarch.arch_gen_nbfilters, [cfgarch.arch_gen_winlen,cfgarch.arch_noise_freqlen], strides=(1, 1), padding='same', dilation_rate=(1, 1), use_bias=False, data_format='channels_last')(l_nm)
                 l_nm = kl.BatchNormalization(axis=bn_axis)(l_nm)
                 l_nm = kl.LeakyReLU(alpha=0.3)(l_nm)
             # l_nm = kl.Dense(vocoder.noisesize(), activation='sigmoid')(l_nm)  # TODO Sigmoid is for PML !
-            l_nm = kl.Conv2D(1, [cfgarch.arch_winlen,cfgarch.arch_noise_freqlen], strides=(1, 1), padding='same', dilation_rate=(1, 1), use_bias=True, activation='sigmoid', data_format='channels_last')(l_nm)
+            l_nm = kl.Conv2D(1, [cfgarch.arch_gen_winlen,cfgarch.arch_noise_freqlen], strides=(1, 1), padding='same', dilation_rate=(1, 1), use_bias=True, activation='sigmoid', data_format='channels_last')(l_nm)
             l_nm = kl.Reshape([-1,l_nm.shape[-2]])(l_nm)
         else:
             l_nm = l_ctx
             for _ in xrange(cfgarch.arch_gen_nbcnnlayers/2):
-                l_nm = kl.Dense(cfgarch.arch_hiddenwidth, use_bias=False)(l_nm)   # Projection
-                l_nm = kl.BatchNormalization(axis=bn_axis)(l_nm)
-                l_nm = kl.LeakyReLU(alpha=0.3)(l_nm)
+                l_nm = networktts.pFC(l_nm, cfgarch.arch_hiddenwidth)
             l_nm = kl.Dense(vocoder.noisesize(), activation='sigmoid')(l_nm)  # TODO Sigmoid is for PML !
 
         l_out = kl.Concatenate(axis=-1)([l_f0, l_spec, l_nm])
